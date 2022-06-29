3459f4b1fac3ecba8657e3b432c1f18405856fd7
Tim N <tim@gurka.se>
2022-06-29 15:26:36 +0200

    supply item properties: type, constlevel, mainlevel, mpath, gemcost

--- api/public_html/inc/Item.php
+++ api/public_html/inc/Item.php
@@ -2,13 +2,15 @@
 
 class Item implements JsonSerializable {
 
+	// SQLite field names, identical to those used in dom5inspector
+	private static $Field_Names = array( 'id', 'name', 'type', 'constlevel', 'mainlevel', 'mpath', 'gemcost' );
+
 	/*
 	 * Constructors
 	 */
 
 	public function __construct(
-		public string $id,
-		public string $name
+		public array  $props
 	) {
 	}
 
@@ -18,11 +20,11 @@ class Item implements JsonSerializable {
 	# return: Item, or null if no matching ID
 	public static function from_id( int $id ) {
 		$db = new SQLite3( 'data/items.db' );
-		$stmt = $db->prepare( 'SELECT id, name FROM items WHERE id=:id' );
+		$stmt = $db->prepare( 'SELECT '.implode(',',Item::$Field_Names).' FROM items WHERE id=:id' );
 		$stmt->bindValue( ':id', $id, SQLITE3_INTEGER );
 		$result = $stmt->execute();
-		if ( $row = $result->fetchArray() ) {
-			$item = new Item( id: $row['id'], name: $row['name'] );
+		if ( $row = $result->fetchArray(SQLITE3_ASSOC) ) {
+			$item = new Item( props: $row );
 		} else {
 			$item = null;
 		}
@@ -36,12 +38,12 @@ class Item implements JsonSerializable {
 	# return: array of Items (empty array if no match)
 	public static function items_with_name( string $name ) :array {
 		$db = new SQLite3( 'data/items.db' );
-		$stmt = $db->prepare( 'SELECT id, name FROM items WHERE name=:name' );
+		$stmt = $db->prepare( 'SELECT '.implode(',',Item::$Field_Names).' FROM items WHERE name=:name' );
 		$stmt->bindValue( ':name', $name, SQLITE3_TEXT );
 		$result = $stmt->execute();
 		$items = array();
-		while ( $row = $result->fetchArray() ) {
-			array_push( $items, new Item( id: $row['id'], name: $row['name'] ) );
+		while ( $row = $result->fetchArray(SQLITE3_ASSOC) ) {
+			array_push( $items, new Item( props: $row ) );
 		}
 		$db->close();
 		return $items;
@@ -54,11 +56,11 @@ class Item implements JsonSerializable {
 	public static function items_with_similar_name( string $name, &$max_similarity ) :array {
 		$needle = strtoupper($name);
 		$db = new SQLite3( 'data/items.db' );
-		$stmt = $db->prepare( 'SELECT id, name FROM items' );
+		$stmt = $db->prepare( 'SELECT '.implode(',',Item::$Field_Names).' FROM items' );
 		$result = $stmt->execute();
 		$max_similarity = -1e6;
 		$rows_at_max = array();
-		while ( $row = $result->fetchArray() ) {
+		while ( $row = $result->fetchArray(SQLITE3_ASSOC) ) {
 			# Finding a proper "similar-text" measure is not straight-forward.
 			# Pull-requests for better alternatives are welcome.
 			$similarity = -round( levenshtein( $needle, strtoupper($row['name']), 1, 100, 100 ) / 5 );
@@ -71,18 +73,23 @@ class Item implements JsonSerializable {
 		}
 		$db->close();
 		$items = array_map(
-			function($row){ return new Item( id: $row['id'], name: $row['name'] ); },
+			function($row){ return new Item( props: $row ); },
 			$rows_at_max
 		);
 		return $items;
 	}
 
+
 	/*
 	 * Getters
 	 */
 
+	public function id() {
+		return $this->props['id'];
+	}
+
 	public function get_screenshot_url() {
-		return sprintf( '/items/%d/screenshot', $this->id );
+		return sprintf( '/items/%d/screenshot', $this->id() );
 	}
 
 
@@ -93,11 +100,10 @@ class Item implements JsonSerializable {
 	 */
 
 	public function jsonSerialize(): array {
-		return [
-			'id' => $this->id,
-			'name' => $this->name,
-			'screenshot' => $this->get_screenshot_url()
-		];
+		return array_merge(
+			$this->props,
+			[ 'screenshot' => $this->get_screenshot_url() ]
+		);
 	}
 
 }
