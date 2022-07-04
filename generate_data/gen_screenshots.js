934d3de06d26cafaaf68b4f75374b2bc6babf6cb
Tim Nordenfur <tim@gurka.se>
2022-07-04 16:14:59 +0200

    inspector event overlay screenshots

---
+++ generate_data/gen_screenshots.js
@@ -0,0 +1,49 @@
+/*
+ * gen_screenshots.js
+ *
+ * Generate screenshots of dom5inspector popups for all items and events.
+ *
+ * Expects dom5inspector running at localhost:8000.
+ */
+
+const puppeteer = require('puppeteer');
+
+
+(async () => {
+	const browser = await puppeteer.launch();
+	const page = await browser.newPage();
+	await page.goto('http://localhost:8000/?loadEvents=1');
+	await page.$('#page-tabs');
+
+
+	for ( const type of ['item', 'event'] ) {
+
+		// Go to correct tab
+		await page.evaluate( (type) => { $('#'+type+'-page-button').trigger('click'); }, type );
+
+		// Get entities (items/events)
+		const overlay = await page.$('#'+type+'-page div.fixed-overlay')
+		const entities = await page.evaluate( (type) => {
+			return Promise.resolve( DMI.modctx[type+'data'] )
+		}, type );
+
+		// Render and capture overlays
+		for ( let entity_i = 0; entity_i < entities.length; entity_i++ ) {
+
+			// render entity to fixed overlay
+			await page.evaluate( (type,entity_i) => {
+				const entity = DMI.modctx[type+'data'][entity_i];
+				$('#'+type+'-page div.fixed-overlay').empty().append( entity.renderOverlay(entity) );
+			}, type, entity_i );
+
+			// sleep waiting for element to unfold: should be a proper await
+			await new Promise(r => setTimeout(r, 500));
+
+			// screenshot overlay
+			const overlay = await page.$('#'+type+'-page div.fixed-overlay')
+			await overlay.screenshot({ path: '../data/screenshot/'+type+'/' + entities[entity_i].id + '.png' });
+		}
+	}
+
+	await browser.close();
+})();
