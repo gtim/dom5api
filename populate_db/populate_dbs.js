5200c00a851085ef04645b1a431e837aea2f5ed2
Tim Nordenfur <tim@gurka.se>
2022-08-06 17:43:11 +0200

    include randompaths (and immobile) properties in units/commanders databases

--- populate_db/populate_dbs.js
+++ populate_db/populate_dbs.js
@@ -102,8 +102,12 @@ async function populate_spells_db( page ) {
 
 async function populate_commanders_db( page ) {
 	const db = new sqlite3.Database('../data/commanders.db' ).serialize();
-	db.run("DROP TABLE IF EXISTS commanders");
-	db.run( fs.readFileSync('commanders.sql').toString() );
+	
+	// Create tables
+	const queries = fs.readFileSync('commanders.sql').toString().split(';').filter( (q) => /\S/.test(q) ); 
+	for ( const query of queries ) {
+		db.run(query);
+	}
 	const units = await page.evaluate(_ => {
 		return Promise.resolve(
 			DMI.modctx.unitdata
@@ -126,7 +130,25 @@ async function populate_commanders_db( page ) {
 		stmt_insert_unit.run( unit );
 	}
 	stmt_insert_unit.finalize();
+
+	// Props table
+	await populate_units_props_table(page, db, 'commander', units.length);
+
 	db.close();
+}
+
+async function populate_units_props_table( page, db, category, num_entities ) {
+	// XXX temporary helper function until "commanders" category is removed
+	const excluded_props = [
+		'id', 'name', 'hp', 'size', // always present, included in commanders table
+		// the following have not yet been handled:
+		'A', 'B', 'D', 'E', 'F', 'H', 'N', 'S', 'W', 'aboleth', 'aciddigest', 'acidshield', 'addrandomage', 'adept_research', 'adeptsacr', 'adventurers', 'ainorec', 'airattuned', 'aisinglerec', 'alchemy', 'allret', 'almostundead', 'ambidextrous', 'amphibian', 'animal', 'animalawe', 'ap', 'appetite', 'aquatic', 'assassin', 'astralattuned', 'astralfetters', 'astralrange', 'att', 'autoblessed', 'autocompete', 'autodishealer', 'autohealer', 'autosum', 'awe', 'banefireshield', 'basecost', 'batstartsum1', 'batstartsum1d3', 'batstartsum1d6', 'batstartsum2', 'batstartsum2d6', 'batstartsum3', 'batstartsum3d6', 'batstartsum4d6', 'batstartsum5d6', 'batstartsum6d6', 'battlesum5', 'beartattoo', 'beastmaster', 'beckon', 'berserk', 'blessbers', 'blessfly', 'blind', 'bloodvengeance', 'bluntres', 'boartattoo', 'body', 'bodyguard', 'bonusspells', 'bringeroffortune', 'bug', 'bugreform', 'carcasscollector', 'casting_enc', 'castledef', 'changetargetgenderforseductionandseductionimmune', 'chaospower', 'chaosrec', 'cheapgod20', 'cheapgod40', 'cleanshape', 'clockworklord', 'cold', 'coldblood', 'coldpower', 'coldrec', 'coldres', 'coldsummon', 'combatcaster', 'combatspeed', 'commaster', 'comslave', 'corpseconstruct', 'corpseeater', 'corrupt', 'createdby', 'crossbreeder', 'crownonly', 'curseattacker', 'curseluckshield', 'damagerev', 'darkpower', 'darkvision', 'deathattuned', 'deathcurse', 'deathdisease', 'deathfire', 'deathparalyze', 'def', 'defector', 'defenceorganiser', 'defiler', 'demon', 'digest', 'disbelieve', 'diseasecloud', 'diseaseres', 'divineins', 'domimmortal', 'domsummon', 'domsummon20', 'doomhorror', 'douse', 'dragonlord', 'drainimmune', 'drake', 'dreanimator', 'dungeon', 'earthattuned', 'elegist', 'elementrange', 'enc', 'enchrebate10', 'enchrebate50', 'entangle', 'eracodes', 'ethereal', 'ethtrue', 'eyeloss', 'eyes', 'fallpower', 'falsearmy', 'farthronekill', 'fear', 'female', 'fireattuned', 'firepower', 'firerange', 'fireres', 'fireshield', 'firstshape', 'fixedname', 'fixedresearch', 'fixforgebonus', 'float', 'flying', 'foot', 'foreignmagicboost', 'forestshape', 'forestsurvival', 'forgebonus', 'formationfighter', 'fortkill', 'fullname', 'gcost', 'gemprod', 'goldcost', 'graphicsize', 'greaterhorror', 'growhp', 'guardianspiritmodifier', 'haltheretic', 'hand', 'head', 'heal', 'heat', 'heathensummon', 'heatrec', 'heretic', 'holy', 'homeshape', 'homesick', 'horrordeserter', 'horrormark', 'horrormarked', 'horsetattoo', 'hpoverflow', 'hpoverslow', 'iceforging', 'iceprot', 'illusion', 'illusionary', 'immortal', 'immortalrespawn', 'inanimate', 'incorporate', 'incunrest', 'indepmove', 'indepspells', 'indepstay', 'inept_research', 'infernoret', 'inquisitor', 'insane', 'insanify', 'inspirational', 'inspiringres', 'invisible', 'invulnerable', 'ironvul', 'isadaeva', 'isashah', 'isayazad', 'ivylord', 'kokytosret', 'labpromotion', 'lamialord', 'lanceok', 'landdamage', 'landenc', 'landshape', 'latehero', 'leader', 'leper', 'lesserhorror', 'linkname', 'listed_mpath', 'localsun', 'magicbeing', 'magicboostA', 'magicboostALL', 'magicboostD', 'magicboostE', 'magicboostF', 'magicboostN', 'magicboostS', 'magicboostW', 'magicleader', 'magicpower', 'magicstudy', 'makepearls', 'mapmove', 'mason', 'mastersmith', 'matchProperty', 'maxage', 'mind', 'mindcollar', 'mindslime', 'mindvessel', 'minprison', 'minsizeleader', 'misc', 'mor', 'moralebonus', 'mountainrec', 'mountainsurvival', 'mounted', 'mountedbeserk', 'mpath', 'mr', 'mummification', 'mummify', 'n_summon', 'nametype', 'nationname', 'natureattuned', 'naturerange', 'neednoteat', 'nobadevents', 'noheal', 'nohof', 'nomovepen', 'norange', 'noriverpass', 'nowish', 'older', 'onebattlespell', 'onisummon', 'pathboost', 'pathboostland', 'pathcost', 'patience', 'patrolbonus', 'percentpathreduction', 'petrify', 'pierceres', 'pillagebonus', 'plaguedoctor', 'plainshape', 'plant', 'poisonarmor', 'poisoncloud', 'poisonres', 'poisonskin', 'polyimmune', 'pooramphibian', 'popkill', 'preanimator', 'prec', 'prophetshape', 'prot', 'raiseonkill', 'raiseshape', 'randomspell', 'rcost', 'rcostsort', 'realms', 'reanimator', 'reanimpriest', 'reclimit', 'recruitedby', 'reformtime', 'regeneration', 'reincarnation', 'reinvigoration', 'renderOverlay', 'reqlab', 'reqtemple', 'researchbonus', 'researchwithoutmagic', 'resources', 'ressize', 'rpcost', 'rt', 'sailingmaxunitsize', 'sailingshipsize', 'sailsize', 'saltvul', 'scalewalls', 'searchable', 'secondshape', 'secondtmpshape', 'seduce', 'sendlesserhorrormult', 'shapechange', 'shatteredsoul', 'shockres', 'shrinkhp', 'siegebonus', 'singlebattle', 'skirmisher', 'slashres', 'slave', 'slaver', 'slaverbonus', 'sleepaura', 'slimer', 'slothresearch', 'slots', 'slow_to_recruit', 'snaketattoo', 'snowmove', 'sorceryrange', 'sorttype', 'speciallook', 'spellsinger', 'spiritsight', 'spreadchaos', 'spreaddeath', 'spreaddom', 'spreadgrowth', 'spreadorder', 'springpower', 'sprite', 'spy', 'standard', 'startaff', 'startage', 'startdom', 'startheroab', 'startingaff', 'startitem', 'stealthy', 'stonebeing', 'stormimmune', 'stormpower', 'str', 'stunimmunity', 'stupid', 'stygianguide', 'succubus', 'summerpower', 'summon', 'summon1', 'summon5', 'summonedby', 'summonedfrom', 'sunawe', 'supplybonus', 'swampsurvival', 'swimming', 'taskmaster', 'taxcollector', 'teleport', 'templetrainer', 'theftofthesunawe', 'thronekill', 'titles', 'tmpastralgems', 'tmpfiregems', 'trample', 'trampswallow', 'transformation', 'triple3mon', 'triplegod', 'triplegodmag', 'turmoilsummon', 'twiceborn', 'type', 'typechar', 'uncurableaffliction', 'undead', 'undeadleader', 'undisciplined', 'undying', 'unify', 'unique', 'unprep', 'unseen', 'unsurr', 'unteleportable', 'uwbug', 'uwdamage', 'uwfireshield', 'uwheat', 'uwregen', 'voidsanity', 'voidsum', 'wastesurvival', 'waterattuned', 'waterbreathing', 'watershape', 'winterpower', 'wintersummon1d3', 'wolf', 'wolftattoo', 'woundfend', 'xploss', 'xpshape', 'yearturn'
+	];
+	const array_props = [];
+	const array_jsonprops = ['randompaths'];
+	const scalar_props = ['immobile'];
+	const unreadable_props = ['armor','cheapgod20','cheapgod40','createdby','dupes','eracodes','nations','recruitedby','sprite','summonedby','summonedfrom','weapons'];
+	await populate_props_table( page, db, category, num_entities, scalar_props, array_props, array_jsonprops, excluded_props, unreadable_props );
 }
 
 /*
@@ -134,10 +156,14 @@ async function populate_commanders_db( page ) {
  */
 
 async function populate_units_db( page ) {
-	// TODO: commanders and units use the same table structure and should be combined
+
 	const db = new sqlite3.Database('../data/units.db' ).serialize();
-	db.run("DROP TABLE IF EXISTS units");
-	db.run( fs.readFileSync('units.sql').toString() );
+
+	// Create tables
+	const queries = fs.readFileSync('units.sql').toString().split(';').filter( (q) => /\S/.test(q) ); 
+	for ( const query of queries ) {
+		db.run(query);
+	}
 	const units = await page.evaluate(_ => {
 		return Promise.resolve(
 			DMI.modctx.unitdata
@@ -155,6 +181,10 @@ async function populate_units_db( page ) {
 		stmt_insert_unit.run( unit );
 	}
 	stmt_insert_unit.finalize();
+	
+	// Props table
+	await populate_units_props_table(page, db, 'unit', units.length);
+
 	db.close();
 }
 
@@ -224,7 +254,6 @@ async function populate_sites_db( page ) {
 
 	// Site props table
 	
-	const stmt_insert_prop = db.prepare("INSERT INTO site_props (site_id, prop_name, arrayprop_ix, value) VALUES ($id, $name, $ix, $value)");
 	const excluded_props = [
 		'id', 'name', 'path', 'level', 'rarity', // always present, included in sites table
 		'renderOverlay', 'matchProperty', 'searchable', 'listed_gempath', 'mpath2', // inspector internals/artifacts
@@ -241,18 +270,48 @@ async function populate_sites_db( page ) {
 		'n_sum1','n_sum2','n_sum3','n_sum4',
 		'aawe','addtolimitedrecruitment','adventure','ageratereduction','airshield','att','awe','bringgold','callgodbonus','coldres','corpselord','curse','darkvision','def','disease','domconflict','domspread','dragonlord','exp','fireres','gold','heal','holyfire','holypow','horror','ivylord','loc','magicresistancebonus','maximizeorder','mor','mpath','natcom','nationalrecruits','natmon','poisonres','prec','reinvigoration','res','reveal','rit','ritrng','rituallevelmodifier','scorch','scry','scryrange','shockres','str','sup','throneclustering','undying','unr','voidgate'
 	];
-	for ( let site_i = 0; site_i < sites.length; site_i++ ) {
-		// Can't grab the entire sitedata array in one go due to puppeteer limitations,
-		// so fetch them one at a time.
-		const props = await page.evaluate( (site_i) => {
-			return Promise.resolve( DMI.modctx.sitedata[site_i] );
-		}, site_i );
+	await populate_props_table( page, db, 'site', sites.length, scalar_props, array_props, [], excluded_props, [] );
+	
+	db.close();
+}
+
+/*
+ * Properties table logic
+ *
+ * scalar_props: stored in single row
+ * array_props: stored one element per row
+ * array_jsonprops: stored one element per row, JSON-encoded
+ * excluded_props: not stored
+ * unreadable_props: cannot be directly serialized by puppeteer and must be culled puppet-side
+ *
+ * TODO: should be better decoupled and more sensible in general
+ *
+ */ 
+
+async function populate_props_table( page, db, category, num_entities, scalar_props, array_props, array_jsonprops, excluded_props, unreadable_props ) {
+	
+	const stmt_insert_prop = db.prepare("INSERT INTO "+category+"_props ("+category+"_id, prop_name, arrayprop_ix, value) VALUES ($id, $name, $ix, $value)");
+
+	if ( category == 'commander' ) { category = 'unit'; } // XXX temporary hack until "commanders" category is removed
+
+	for ( let site_i = 0; site_i < num_entities; site_i++ ) {
+		// Can't grab the entire array in one go due to puppeteer limitations, so fetch them one at a time.
+		const props = await page.evaluate( (category, entity_i, unreadable_props) => {
+			let entity = DMI.modctx[category+'data'][entity_i];
+			for ( const unreadable_prop of unreadable_props ) {
+				delete entity[unreadable_prop];
+			}
+			return Promise.resolve( entity );
+		}, category, site_i, unreadable_props );
 		for ( const prop_name of Object.keys(props) ) {
 			if ( excluded_props.includes(prop_name) ) {
 				continue;
-			} else if ( array_props.includes(prop_name) ) {
+			} else if ( array_props.includes(prop_name) || array_jsonprops.includes(prop_name) ) {
 				assert.ok( props[prop_name].constructor === Array, 'expected array prop' );
-				for ( const [arrayprop_ix, arrayprop_element ] of props[prop_name].entries() ) {
+				for ( let [arrayprop_ix, arrayprop_element ] of props[prop_name].entries() ) {
+					if ( array_jsonprops.includes(prop_name) ) {
+						arrayprop_element = JSON.stringify(arrayprop_element);
+					}
 					stmt_insert_prop.run( { $id: props.id, $name: prop_name, $value: arrayprop_element, $ix: arrayprop_ix });
 				}
 			} else if ( scalar_props.includes(prop_name ) ) {
@@ -266,6 +325,4 @@ async function populate_sites_db( page ) {
 		}
 	}
 	stmt_insert_prop.finalize();
-	
-	db.close();
 }
--- populate_db/units.sql
+++ populate_db/units.sql
@@ -1,20 +1,20 @@
-DROP TABLE IF EXISTS commanders;
+DROP TABLE IF EXISTS units;
 
-CREATE TABLE commanders (
+CREATE TABLE units (
 	id         INT  NOT NULL PRIMARY KEY,
 	name       TEXT NOT NULL COLLATE NOCASE,
 	hp         INT  NOT NULL,
 	size       INT  NOT NULL CHECK( size >= 1 AND size <= 6 )
 );
 
-DROP TABLE IF EXISTS commander_props;
+DROP TABLE IF EXISTS unit_props;
 
-CREATE TABLE commander_props (
-	commander_id  INT NOT NULL,
+CREATE TABLE unit_props (
+	unit_id  INT NOT NULL,
 	prop_name     TEXT NOT NULL,
 	value         TEXT NOT NULL,
 	arrayprop_ix  INT,
-	PRIMARY KEY ( commander_id, prop_name, arrayprop_ix ),
-	FOREIGN KEY (commander_id) REFERENCES commanders(id)
+	PRIMARY KEY ( unit_id, prop_name, arrayprop_ix ),
+	FOREIGN KEY (unit_id) REFERENCES units(id)
 		ON DELETE CASCADE
 );
